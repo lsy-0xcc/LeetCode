758963a0f24855de42632615dd449a96dff6403f
lsy0xcc <lsy0xcc@outlook.com>
2020-11-01 23:33:05 +0800

    :sparkles: 20201101

--- leetcode/editor/cn/Q381InsertDeleteGetrandomO1DuplicatesAllowed.java
+++ leetcode/editor/cn/Q381InsertDeleteGetrandomO1DuplicatesAllowed.java
@@ -41,71 +41,110 @@ import java.util.*;
 //Java：O(1) 时间插入、删除和获取随机元素 - 允许重复
 public class Q381InsertDeleteGetrandomO1DuplicatesAllowed {
     public static void main(String[] args) {
-//        RandomizedCollection solution = new Q381InsertDeleteGetrandomO1DuplicatesAllowed().new RandomizedCollection();
-        // test
+        RandomizedCollection randomizedCollection = new RandomizedCollection();
+//        System.out.println(randomizedCollection.remove(1));
+//        System.out.println(randomizedCollection.insert(1));
+//        System.out.println(randomizedCollection.insert(1));
+//        System.out.println(randomizedCollection.remove(1));
+//        System.out.println(randomizedCollection.insert(1));
+//        System.out.println(randomizedCollection.remove(1));
+        System.out.println(randomizedCollection.insert(1));
+        System.out.println(randomizedCollection.insert(1));
+        System.out.println(randomizedCollection.insert(2));
+        System.out.println(randomizedCollection.insert(1));
+        System.out.println(randomizedCollection.insert(2));
+        System.out.println(randomizedCollection.insert(2));
+        System.out.println(randomizedCollection.remove(1));
+        System.out.println(randomizedCollection.remove(2));
+        System.out.println(randomizedCollection.remove(2));
+        System.out.println(randomizedCollection.remove(2));
+        System.out.println(randomizedCollection.remove(2));
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println(randomizedCollection.getRandom());
+        System.out.println();
     }
 
-    //leetcode submit region begin(Prohibit modification and deletion)
 
 
-//    class RandomizedCollection {
-//        List<Integer> list;
-//        Map<Integer, Set<Integer>> positions;
-//        /**
-//         * Initialize your data structure here.
-//         */
-//        public RandomizedCollection() {
-//            list = new ArrayList<>();
-//            positions = new HashMap<>();
-//        }
-//
-//        /**
-//         * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
-//         */
-//        public boolean insert(int val) {
-//            list.add(val);
-//            if (!positions.containsKey(val)) {
-//                positions.put(val, new HashSet<>());
-//                positions.get(val).add(list.size() - 1);
-//                return true;
-//            } else {
-//                positions.get(val).add(list.size() - 1);
-//                return false;
-//            }
-//        }
-//
-//        /**
-//         * Removes a value from the collection. Returns true if the collection contained the specified element.
-//         */
-//        public boolean remove(int val) {
-//            if (!positions.containsKey(val)) {
-//                return false;
-//            } else {
-//                int removePos =-1;
-//                for (Integer e : positions.get(val)) {
-//                    removePos = e;
-//                    break;
-//                }
-//                positions.get(val).remove(removePos);
-//                if (positions.get(val).size() == 0) {
-//                    positions.remove(val);
-//                }
-//                int lastNum = list.get(list.size() - 1);
-//                positions.get(lastNum).add(removePos);
-//                list.set(removePos, lastNum);
-//                list.remove(list.size() - 1);
-//                return true;
-//            }
-//        }
-//
-//        /**
-//         * Get a random element from the collection.
-//         */
-//        public int getRandom() {
-//            int pos = (int) (Math.random() * list.size());
-//            return list.get(pos);
-//        }
-//    }
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+
+class RandomizedCollection {
+    // 用一个数组保存所有元素
+    List<Integer> list;
+    // key: list中元素
+    // value: key在list中出现位置
+    Map<Integer, Set<Integer>> positions;
+    /**
+     * Initialize your data structure here.
+     */
+    public RandomizedCollection() {
+        list = new ArrayList<>();
+        positions = new HashMap<>();
+    }
+
+    /**
+     * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
+     */
+    public boolean insert(int val) {
+        list.add(val); // 向list中添加
+        if (!positions.containsKey(val)) {
+            positions.put(val, new HashSet<>());
+            positions.get(val).add(list.size() - 1);
+            return true;
+        } else {
+            positions.get(val).add(list.size() - 1);
+            return false;
+        }
+    }
+
+    /**
+     * Removes a value from the collection. Returns true if the collection contained the specified element.
+     */
+    public boolean remove(int val) {
+
+        if (!positions.containsKey(val)) {
+            return false;
+        } else {
+            // 选取val对应set中一个元素移除
+            int removePos =-1;
+            for (Integer e : positions.get(val)) {
+                removePos = e;
+                break;
+            }
+            // 移除position中对应的位置
+            positions.get(val).remove(removePos);
+            // 找到list中最后一个元素与要移除的交换
+            int lastNum = list.get(list.size() - 1);
+            // 最后一个元素的位置由最后到移除的位置
+            positions.get(lastNum).add(removePos);
+            positions.get(lastNum).remove(list.size() - 1);
+            // 若被移除的元素不存在于list中 移除
+            if (positions.get(val).size() == 0) {
+                positions.remove(val);
+            }
+            // 修改list
+            list.set(removePos, lastNum);
+            list.remove(list.size() - 1);
+            return true;
+        }
+    }
+
+    /**
+     * Get a random element from the collection.
+     */
+    public int getRandom() {
+        int pos = (int) (Math.random() * list.size());
+        return list.get(pos);
+    }
+}
 
 
 /**
@@ -116,5 +155,3 @@ public class Q381InsertDeleteGetrandomO1DuplicatesAllowed {
  * int param_3 = obj.getRandom();
  */
 //leetcode submit region end(Prohibit modification and deletion)
-
-}
